2e94c0d56f0478c568f9d6f09c5288dff3f18586
Lucas Costa <lucas.hcruz.costa@gmail.com>
2021-07-03 19:21:36 -0300

    refacotr: add helmet

--- api-identity/src/app/http/kernel.js
+++ api-identity/src/app/http/kernel.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
 
 const routes = require('../routes');
 const errorMiddleware = require('./middlewares/error-middleware');
 
 const app = express();
 
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(routes);
