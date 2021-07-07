8f5ae629fe8a0a5e618f346c14d66efc95cdefda
Lucas Costa <lucas.hcruz.costa@gmail.com>
2021-07-07 20:10:47 -0300

    feat: update transaction status

--- api-baas/app/Listeners/TransactionFailed.php
+++ api-baas/app/Listeners/TransactionFailed.php
@@ -2,7 +2,9 @@
 
 namespace App\Listeners;
 
+use App\Enums\TransactionStatusEnum;
 use App\Listeners\Contracts\ITransactionListener;
+use App\Repositories\Contracts\ITransactionRepository;
 use App\Repositories\Contracts\IWalletRepository;
 
 class TransactionFailed implements ITransactionListener
@@ -10,10 +12,16 @@ class TransactionFailed implements ITransactionListener
     public function process($payload)
     {
         $walletRepository = app()->make(IWalletRepository::class);
+        $transactionrepository = app()->make(ITransactionRepository::class);
         $walletRepository->increment(
             $payload['wallet_from'],
             'balance',
             (float) $payload['total']
+        );
+
+        $transactionrepository->update(
+            $payload['transaction'], 
+            ['status' => TransactionStatusEnum::FAILED]
         );
     }
 }
--- api-baas/app/Listeners/TransactionSuccess.php
+++ api-baas/app/Listeners/TransactionSuccess.php
@@ -2,7 +2,9 @@
 
 namespace App\Listeners;
 
+use App\Enums\TransactionStatusEnum;
 use App\Listeners\Contracts\ITransactionListener;
+use App\Repositories\Contracts\ITransactionRepository;
 use App\Repositories\Contracts\IWalletRepository;
 
 class TransactionSuccess implements ITransactionListener
@@ -10,10 +12,16 @@ class TransactionSuccess implements ITransactionListener
     public function process($payload)
     {
         $walletRepository = app()->make(IWalletRepository::class);
+        $transactionrepository = app()->make(ITransactionRepository::class);
         $walletRepository->increment(
             $payload['wallet_to'],
             'balance',
             (float) $payload['total']
         );
-    }
+
+        $transactionrepository->update(
+            $payload['transaction'], 
+            ['status' => TransactionStatusEnum::SUCCESS]
+        );
+    }   
 }
