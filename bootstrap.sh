7a035e4e7dfaddd32c0d30322cb1fb60191cfca9
Lucas Costa <lucas.hcruz.costa@gmail.com>
2021-07-07 20:45:57 -0300

    refactor: run docker compose with build image

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Script to up containers, run database migrations, run command to listen rabbitmq
 
-docker-compose up -d
+docker-compose up -d --build
 docker exec codepix_api_baas php artisan migrate --seed
 sleep 20 
 docker exec codepix_api_baas nohup php artisan message-broker:consume transactions_response > /dev/null 2>&1 &
